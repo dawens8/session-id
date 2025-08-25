537dc04d102a9caa5d381429d8a1068355f77106
dawens8 <mydawens8@gmail.com>
2025-08-25 10:49:33 -0400

    Update id.js

--- id.js
+++ id.js
@@ -1,11 +1,24 @@
-function malvinid(num = 4) {
-  let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let characters9 = characters.length;
-  for (let i = 2; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
-  }
-  return result;
+Checkpoint-Computer -Description "Before-WhatsApp-Hardening" -RestorePointType "MODIFY_SETTINGS"
+
+Set-MpPreference -DisableRealtimeMonitoring $false
+Set-MpPreference -MAPSReporting Advanced
+Set-MpPreference -SubmitSamplesConsent SendSafeSamples
+
+Set-MpPreference -EnableControlledFolderAccess Enabled
+
+$waPaths = @(
+  "$env:USERPROFILE\AppData\Roaming\WhatsApp",
+  "$env:LOCALAPPDATA\Packages\WhatsApp.WhatsApp*\LocalState",
+  "$env:LOCALAPPDATA\Packages\5319275A.WhatsAppDesktop*\LocalState"
+)
+foreach ($p in $waPaths) {
+  try { Add-MpPreference -ControlledFolderAccessProtectedFolders $p } catch {}
 }
 
-module.exports = { malvinid };
+Start-MpScan -ScanType QuickScan
+
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+
+$trg = New-ScheduledTaskTrigger -Daily -At 12:00
+$act = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "Start-MpScan -ScanType QuickScan"
+Register-ScheduledTask -TaskName "DailyDefenderQuickScan" -Trigger $trg -Action $act -RunLevel Highest -Description "Daily quick scan via Microsoft Defender" -Force
